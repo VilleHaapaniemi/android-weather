99428619e832fa777537ad116df6b69743d01d0b
Ville Haapaniemi <ville.haapaniemi2@gmail.com>
2023-11-28 13:15:36 +0200

    Created Main and Settings activities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.android_weather"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.android_weather"
         minSdk = 27
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
